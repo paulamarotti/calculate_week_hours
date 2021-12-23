fd0e4f279acc260dc16d141d3e536c5dd9bc0bd3
Paula Marotti <paula.marotti@Paula.Marotti>
2021-12-23 11:11:27 -0300

    calculates week hours and prints to the terminal

---
+++ week_hours.py
@@ -0,0 +1,10 @@
+#python aula 2
+#calculation of hours in a week
+
+hours_day = 24
+days_week = 7
+
+#variable that calculates how many hours there are in a week
+week_hours = hours_day * days_week
+
+print(week_hours)
